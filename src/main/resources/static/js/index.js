7a188f13b842061d5ae10b17ad501f046dda6e1f
GiYeons <65033360+GiYeons@users.noreply.github.com>
2023-12-03 17:53:52 +0900

    feat: 스트리밍 시작/중단 버튼 만들기

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,58 +1,96 @@
-document.addEventListener("DOMContentLoaded", () => {
-    new App();
-})
+const startWebcamStream = async () => {
+    try {
+        // 스트리밍 시작 버튼 숨기기
+        toggleBtn(document.getElementById('startWebcamBtn'));
 
-class App {
-    constructor() {
+        // 스트리밍 중단 버튼 보이기
+        toggleBtn(document.getElementById('stopWebcamBtn'));
 
-        const video = document.getElementById('cameraview');
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const video = document.getElementById('webcamVideo');
+        video.srcObject = stream;
 
-        if (navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then( (stream) => { // function 의 this와 화살표 함수의 this 가 다름
-                    video.srcObject = stream;
-                })
-                .catch(function (error) {
-                    console.log("Something went wrong!");
-                    console.log(error);
-                    return;
-                });
+        // 비디오 메타데이터 로드를 기다림
+        video.addEventListener('loadedmetadata', () => {
+            // 비디오가 로드된 후에 캡처 시작
+            captureFrameAndSend();
+        });
+    } catch (error) {
+        console.error('Error accessing webcam:', error);
+    }
+};
+
+
+const captureFrameAndSend = async () => {
+    const video = document.getElementById('webcamVideo');
+
+    // 비디오 크기 확인
+    if (video.videoWidth === 0 || video.videoHeight === 0) {
+        console.warn('Video size is 0. Waiting for metadata.');
+        return;
+    }
+
+    const canvas = document.getElementById('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = new Uint8Array(imageData.data);
+
+    // 웹캠 영상 데이터를 Flask로 전송
+    sendWebcamStream(data);
+
+    // 반복 호출
+    requestAnimationFrame(captureFrameAndSend);
+};
+
+
+const sendWebcamStream = async (webcamData) => {
+    const url = 'http://localhost:5000/api/receiveWebcamStream';
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/octet-stream',
+            },
+            body: webcamData,
+        });
+
+        if (!response.ok) {
+            console.error('Error sending webcam stream:', response.statusText);
         }
-
-        video.addEventListener( "loadedmetadata", () => {
-            window.requestAnimationFrame(this.draw.bind(this));
-        });
+    } catch (error) {
+        console.error('Error sending webcam stream:', error);
     }
+};
 
-    draw(t) {
+const stopWebcam = async () => {
+    const video = document.getElementById('webcamVideo');
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
 
-        window.requestAnimationFrame(this.draw.bind(this));
+    const stream = video.srcObject;
+    const tracks = stream.getTracks();
 
-        const canvas = document.getElementById('canvas');
-        const canvas2 = document.getElementById('canvas2');
-        const video = document.getElementById('cameraview');
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+    tracks.forEach(track => track.stop());
+    video.srcObject = null;
 
-        const ctx = canvas.getContext('2d');
-        const ctx2 = canvas2.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-        ctx.font = '20px serif';
+    // 스트리밍 시작 버튼 숨기기
+    toggleBtn(document.getElementById('startWebcamBtn'));
 
-        ctx.fillText('비디오 로딩 중..', canvas.width / 2, canvas.height / 2);
-        ctx.translate(video.videoWidth, 0);
-        ctx.scale(-1,1);
-        ctx.drawImage(video, 0, 0,
-            video.videoWidth,
-            video.videoHeight);
+    // 스트리밍 중단 버튼 보이기
+    toggleBtn(document.getElementById('stopWebcamBtn'));
+};
 
-        const img = document.createElement('img');
-        img.src = '/images/test.png';
-        img.addEventListener('load', () => {
-            ctx.drawImage(img, 100, 100, 100, 100); // 그릴 이미지 엘리먼트, x, y, width ,height
-            ctx2.drawImage(img, 100, 100, 100, 100); // 그릴 이미지 엘리먼트, x, y, width ,height
-        });
-
-        ctx2.fillText('비디오 로딩 중..', canvas.width / 2, canvas.height / 2);
+function toggleBtn(btn) {
+    if(btn.style.display != 'none') {
+        btn.style.display = 'none';
     }
-}
+    else {
+        btn.style.display = 'inline';
+    }
+};
